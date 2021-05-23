6eebb870f0e05ea042e982878cdc52c6cdd29787
agrufu <69220569+agrufu@users.noreply.github.com>
2021-05-23 23:49:56 +0200

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,31 @@
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xmlhttp.open("GET", "pedido.xml", true);
+  xmlhttp.send();
+}
+
+function myFunction(xml) {
+  var x, i, xmlDoc, table;
+  xmlDoc = xml.responseXML;
+  table = "<tr><th>Nombre</th><th>Marca</th><th>Color</th><th>Cantidad</th></tr>";
+
+  x = xmlDoc.getElementsByTagName("articulo");
+  for (i = 0; i < x.length; i++) {
+    table +=
+      "<tr><td>" +
+      x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("marca")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("color")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("cantidad")[0].childNodes[0].nodeValue +
+      "</td></tr>";
+  }
+  document.getElementById("pedido").innerHTML = table;
+}
